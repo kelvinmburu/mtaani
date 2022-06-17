82afc3de93169cd5d8706efb4295281bedcdb644
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-17 23:20:27 +0300

    feat: added path for tailwind reload server

--- mtaaniproj/urls.py
+++ mtaaniproj/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('mtaaniapp.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
 if(settings.DEBUG):
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
