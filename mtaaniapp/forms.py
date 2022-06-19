75b01c1b3ff070e007bcc6019dc6df2ad8e7e051
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-19 12:04:20 +0300

    feat: added business and post form for neigborhood field

--- mtaaniapp/forms.py
+++ mtaaniapp/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, NeighbourHood, Business, Post
-from pyuploadcare.dj.forms import ImageField
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=255, help_text='Required. Inform a valid')
@@ -18,5 +17,15 @@ class UpdateProfileForm(forms.ModelForm):
         
 class NeighbourhoodForm(forms.ModelForm):
     class Meta:
-        model = Business
+        model = NeighbourHood
         exclude = ('admin')
+        
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
