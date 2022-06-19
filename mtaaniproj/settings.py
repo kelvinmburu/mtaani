43448b8156bd75a833d9edd06a6b360d2ae47cdc
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-20 02:49:35 +0300

    fix: fixed typo

--- mtaaniproj/settings.py
+++ mtaaniproj/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
